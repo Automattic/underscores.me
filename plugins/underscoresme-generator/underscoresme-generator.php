b8316ca80a2dc06d6ae085415efdd139790de60f
Brad Parbs <brad@bradparbs.com>
2014-04-27 13:39:39 -0400

    Also replace theme headers in Style.scss, not just style.css props @jtsterberg

--- plugins/underscoresme-generator/underscoresme-generator.php
+++ plugins/underscoresme-generator/underscoresme-generator.php
@@ -179,7 +179,7 @@ class Underscores_Generator_Plugin {
 			return $contents;
 
 		// Special treatment for style.css
-		if ( 'style.css' == $filename ) {
+		if( in_array( $file, array('style.css', 'style.scss'), true ) ){
 			$theme_headers = array(
 				'Theme Name'  => $this->theme['name'],
 				'Theme URI'   => esc_url_raw( $this->theme['uri'] ),
