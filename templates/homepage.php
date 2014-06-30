bef4c2ab1063af6aff4464e404a0de40ef6c0c80
Lucas Cherkewski <lucas@ecustom.ca>
2014-06-30 19:36:41 -0400

    Add trailing slash to `layouts`.

--- templates/homepage.php
+++ templates/homepage.php
@@ -32,7 +32,7 @@ get_header(); ?>
 						<li>Custom template tags in <code>inc/template-tags.php</code> that keep your templates clean and neat and prevent code duplication.</li>
 						<li>Some small tweaks in <code>inc/extras.php</code> that can improve your theming experience.</li>
 						<li>A script at <code>js/navigation.js</code> that makes your menu a toggled dropdown on small screens (like your phone), ready for CSS artistry.</li>
-						<li>2 sample CSS layouts in <code>layouts</code> for a sidebar on either side of your content.</li>
+						<li>2 sample CSS layouts in <code>layouts/</code> for a sidebar on either side of your content.</li>
 						<li>Smartly organized starter CSS in <code>style.css</code> that will help you to quickly get your design off the ground.</li>
 						<li>Licensed under GPLv2 or later. :) Use it to make something cool.</li>
 					</ul><!-- #features -->
