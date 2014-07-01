c89f9161582f72eae524a8fe6566d655b806d178
Jack Lenox <jack.lenox@gmail.com>
2014-07-01 12:22:52 +0100

    Added a favicon.ico and reference in header.php

--- header.php
+++ header.php
@@ -35,6 +35,7 @@
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
