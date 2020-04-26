b89b73a392655913004c7842940202c73362754a
Ismail El Korchi <ismail.elkorchi@gmail.com>
2020-04-26 23:27:12 +0000

    Remove dead code

--- functions.php
+++ functions.php
@@ -36,25 +36,6 @@ function underscoresme_setup() {
 	 */
 	require( get_template_directory() . '/plugins/underscoresme-generator/underscoresme-generator.php' );
 
-	// Remove me later:
-	//if ( ! is_admin() && ! isset( $_REQUEST['underscoresme_generate'] ) )
-	//	do_action( 'underscoresme_print_form' );
-
-	/**
-	 * Custom functions that act independently of the theme templates
-	 */
-	//require( get_template_directory() . '/inc/tweaks.php' );
-
-	/**
-	 * Custom Theme Options
-	 */
-	//require( get_template_directory() . '/inc/theme-options/theme-options.php' );
-
-	/**
-	 * WordPress.com-specific functions and definitions
-	 */
-	//require( get_template_directory() . '/inc/wpcom.php' );
-
 	/**
 	 * Make theme available for translation
 	 * Translations can be filed in the /languages/ directory
@@ -64,46 +45,12 @@ function underscoresme_setup() {
 	load_theme_textdomain( 'underscoresme', get_template_directory() . '/languages' );
 
 	/**
-	 * Add default posts and comments RSS feed links to head
-	 */
-	//add_theme_support( 'automatic-feed-links' );
-
-	/**
 	 * Enable support for Post Thumbnails
 	 */
 	add_theme_support( 'post-thumbnails' );
-
-	/**
-	 * This theme uses wp_nav_menu() in one location.
-	 */
-	/*register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'underscoresme' ),
-	) );*/
-
-	/**
-	 * Add support for the Aside Post Formats
-	 */
-	//add_theme_support( 'post-formats', array( 'aside', ) );
 }
 endif; // underscoresme_setup
 add_action( 'after_setup_theme', 'underscoresme_setup' );
-
-/**
- * Register widgetized area and update sidebar with default widgets
- *
- * @since Underscores.me 1.0
- */
-function underscoresme_widgets_init() {
-	register_sidebar( array(
-		'name' => __( 'Sidebar', 'underscoresme' ),
-		'id' => 'sidebar-1',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget' => "</aside>",
-		'before_title' => '<h1 class="widget-title">',
-		'after_title' => '</h1>',
-	) );
-}
-//add_action( 'widgets_init', 'underscoresme_widgets_init' );
 
 /**
  * Enqueue scripts and styles
@@ -113,23 +60,8 @@ function underscoresme_scripts() {
 
 	wp_enqueue_style( 'style', add_query_arg( 'v', 20140811, get_stylesheet_uri() ) );
 	wp_enqueue_script( 'underscores-scripts', get_template_directory_uri() . '/js/underscores-scripts.js', array( 'jquery' ), '20120813' );
-	
-	/*wp_enqueue_script( 'small-menu', get_template_directory_uri() . '/js/small-menu.js', array( 'jquery' ), '20120206', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-
-	if ( is_singular() && wp_attachment_is_image( $post->ID ) ) {
-		wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
-	}*/
 }
 add_action( 'wp_enqueue_scripts', 'underscoresme_scripts' );
-
-/**
- * Implement the Custom Header feature
- */
-//require( get_template_directory() . '/inc/custom-header.php' );
 
 /**
  * Returns an array of contributors from Github.
--- header.php
+++ header.php
@@ -36,9 +36,6 @@
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" />
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
 
 <?php //TYPEKIT CODE - Konstantin, can you please move this to the enqueue scripts section in the functions.php file? ?>
 <script type="text/javascript" src="//use.typekit.net/qcr7ewy.js"></script>
